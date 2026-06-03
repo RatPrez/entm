643f39faa5f1826afd48dc7d6708c4a43a381608
Ratprez <41370482+RatPrez@users.noreply.github.com>
2026-06-04 04:31:15 +1000

    fix: changed type of entityType to number

--- src/shared/components/CfxEntity.ts
+++ src/shared/components/CfxEntity.ts
@@ -3,10 +3,10 @@ import { Component } from "../core/Component";
 export class CfxEntity extends Component {
     readonly handle:     number;
     readonly model:      number;
-    readonly entityType: string;
+    readonly entityType: number;
     readonly netId:      number | null;
 
-    constructor(handle: number, model: number, entityType: string, netId: number | null) {
+    constructor(handle: number, model: number, entityType: number, netId: number | null) {
         super();
         this.handle     = handle;
         this.model      = model;
